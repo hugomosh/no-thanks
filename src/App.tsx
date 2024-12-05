73a6193cc901d851b35980e7614492778574ce18
Hugo Mosh Cardoza <hugomosh@gmail.com>
2024-12-05 07:12:36 +0000

    Update app to work with github pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { JoinPage } from "./pages/JoinPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="" element={<HomePage />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/room/:roomCode" element={<RoomPage />} />
       </Routes>
